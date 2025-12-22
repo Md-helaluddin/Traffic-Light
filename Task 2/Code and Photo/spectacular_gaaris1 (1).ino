fe25f1bb57b341e47535c07c8035c6c334d3bbff
Md-helaluddin <md-helal.uddin@stud.hshl.de>
2025-12-22 22:42:19 +0100

    conflict changes in the code

--- Task 2/Code and Photo/spectacular_gaaris1 (1).ino
+++ Task 2/Code and Photo/spectacular_gaaris1 (1).ino
@@ -82,23 +82,15 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(2), handleButtonPress, FALLING);
 }
 
-<<<<<<< HEAD:Task 4/4 conde traffic.ino
 const unsigned long tGreen = 7000;
 const unsigned long tYellow = 3000;
 const unsigned long tRed = 6000;
-=======
-const unsigned long tGreen = 5000;
-const unsigned long tYellow = 2000;
-const unsigned long tRed = 5000;
->>>>>>> 596d2d783d69b0594305ce8e9b7decd81a8829e5:Task 4/4 code.ino
 
 int click;
 
 void loop() {   
     mainPart();
 }
-
-
 
 void mainPart() {
   unsigned long currentTime = millis();
@@ -114,39 +106,20 @@ void mainPart() {
 
   switch (carState) {
     case Green:
-<<<<<<< HEAD:Task 4/4 conde traffic.ino
-  carLight.greenOn();
-  pedLight.redOn();
-
-  if (waiting) {  
-    carState = Yellow;
-    lastChangeTime = currentTime; 
-    click = 5;
-  } 
-  else if (TimeCheck >= tGreen) { 
-    carState = Yellow;
-    lastChangeTime = currentTime;
-  }
-  StateDetector = 0;
-  break;
-
-=======
       carLight.greenOn();
       pedLight.redOn();
-    
-      if (TimeCheck >= tGreen) {
+
+      if (waiting) {  
+        carState = Yellow;
+        lastChangeTime = currentTime; 
+        click = 5;
+      } 
+      else if (TimeCheck >= tGreen) { 
         carState = Yellow;
         lastChangeTime = currentTime;
       }
-      
-      if(waiting){
-          
-          carState = Yellow;
-          click = 5;
-      }
       StateDetector = 0;
       break;
->>>>>>> 596d2d783d69b0594305ce8e9b7decd81a8829e5:Task 4/4 code.ino
 
     case Yellow:
       carLight.yellowOn();
@@ -156,22 +129,16 @@ void mainPart() {
         carState = (StateDetector == 0) ? Red : Green;
         lastChangeTime = currentTime;
       }
-      
       break;
 
     case Red:
-      
-      if(click == 5)
-      {
-          carLight.redOn();
-          pedLight.greenOn();
+      if (click == 5) {
+        carLight.redOn();
+        pedLight.greenOn();
       }
-      else 
-      {
-          carLight.redOn();
-          
+      else {
+        carLight.redOn();
       }
-      
     
       if (TimeCheck >= tRed) {
         carState = Yellow;
